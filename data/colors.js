46db53bd066fb2d10b7208279ea32757b10cc8df
jlandis320 <jreynoldslandis@gmail.com>
2022-09-09 16:07:06 -0400

    data set complete

--- data/colors.js
+++ data/colors.js
@@ -31,17 +31,154 @@ const oranges = [
   "orange",
 ]
 // length = 6
-console.log(oranges.length);
 
 const yellows = [
-  ""
+  "gold",
+  "yellow",
+  "lightyellow",
+  "lemonchiffon",
+  "lightgoldenrodyellow",
+  "papayawhip",
+  "moccasin",
+  "peachpuff",
+  "palegoldenrod",
+  "khaki",
+  "darkkhaki"
 ]
-const greens = []
-const blues = []
-const purples = []
-const browns = []
-const whites = []
-const grays = []
+// length = 11
+console.log(yellows.length + 'yellows');
+const greens = [
+  "greenyellow",
+  "chartreuse",
+  "lawngreen",
+  "lime",
+  "limegreen",
+  "palegreen",
+  "lightgreen",
+  "mediumspringgreen",
+  "springgreen",
+  "mediumseagreen",
+  "seagreen",
+  "forestgreen",
+  "green",
+  "darkgreen",
+  "yellowgreen",
+  "olivedrab",
+  "olive",
+  "darkolivegreen",
+  "mediumaquamarine",
+  "darkseagreen",
+  "lightseagreen",
+  "darkcyan",
+  "teal",
+]
+// length = 23 
+const blues = [
+  "aqua",
+  "cyan",
+  "lightcyan",
+  "paleturquoise",
+  "aquamarine",
+  "turquoise",
+  "mediumturquoise",
+  "darkturquoise",
+  "cadetblue",
+  "steelblue",
+  "lightsteelblue",
+  "powderblue",
+  "lightblue",
+  "skyblue",
+  "lightskyblue",
+  "deepskyblue",
+  "dodgerblue",
+  "cornflowerblue",
+  "mediumslateblue",
+  "royalblue",
+  "blue",
+  "mediumblue",
+  "darkblue",
+  "navy",
+  "midnightblue",
+]
+// length = 25
+
+const purples = [
+  "lavender",
+  "thistle",
+  "plum",
+  "violet",
+  "orchid",
+  "fuchsia",
+  "magenta",
+  "mediumorchid",
+  "mediumpurple",
+  "rebeccapurple",
+  "blueviolet",
+  "darkviolet",
+  "darkorchid",
+  "darkmagenta",
+  "purple",
+  "indigo",
+  "slateblue",
+  "darkslateblue",
+  "mediumslateblue"
+]
+// length = 19
+
+const browns = [
+  "cornsilk",
+  "blanchedalmond",
+  "bisque",
+  "navajowhite",
+  "wheat",
+  "burlywood",
+  "tan",
+  "rosybrown",
+  "sandybrown",
+  "goldenrod",
+  "darkgoldenrod",
+  "peru",
+  "chocolate",
+  "saddlebrown",
+  "sienna",
+  "brown",
+  "maroon",
+]
+// length = 17
+const whites = [
+  "white",
+  "snow",
+  "honeydew",
+  "mintcream",
+  "azure",
+  "aliceblue",
+  "ghostwhite",
+  "whitesmoke",
+  "seashell",
+  "beige",
+  "oldlace",
+  "floralwhite",
+  "ivory",
+  "antiquewhite",
+  "linen",
+  "lavenderblush",
+  "mistyrose",
+]
+// length = 17
+const grays = [
+  "gainsboro",
+  "lightgray",
+  "silver",
+  "darkgray",
+  "gray",
+  "dimgray",
+  "lightslategray",
+  "slategray",
+  "darkslategray",
+  "black",
+]
+// length = 10
+console.log(reds.length + pinks.length + oranges.length + yellows.length + greens.length + blues.length + purples.length + browns.length + whites.length + grays.length);
 
 // export {
 //   reds,
