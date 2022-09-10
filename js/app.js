b10d93b1df0fa44ac54f98479e115231cff3b0fd
jlandis320 <jreynoldslandis@gmail.com>
2022-09-10 13:06:31 -0400

    adds favicon

--- js/app.js
+++ js/app.js
@@ -1,3 +1,62 @@
-import { reds, } from "../data/colors.js"
+// import { reds, } from "../data/colors.js"
 
-console.log(reds);
+// 
+
+/*-------------------------------- Constants --------------------------------*/
+// all color arrays
+
+
+
+/*-------------------------------- Variables --------------------------------*/
+// currentColor
+// colorOptions (+currentColor)= []
+
+// usedColors = []
+// score
+// timeElapsed
+// correctAnswerChosen ?
+setTimeout(() => { console.log("hello")}, 2000)
+
+/*------------------------ Cached Element References ------------------------*/
+
+
+
+/*----------------------------- Event Listeners -----------------------------*/
+
+
+
+/*-------------------------------- Functions --------------------------------*/
+// function increaseScore - += 1
+// function colorPicker(category, numColors)
+// function currentColorPicker 
+// function colorOptionsPicker
+// handleClick for category
+// handleClick for buttons
+// renderFunction = set color name to ?, set the color swatch to white, set the timer to 15s, score to 0
+
+
+/*-------------------------------- Pseudocode -------------------------------*/
+
+// // Create data file with category arrays: warms, cools, grays, white
+// * Render function: categories display with score display, empty paint swatch with empty button choices
+//     * Need a color wheel divided into 10 quadrants — add event listener to each quadrant
+
+
+// * When clicked, paint swatch changes to random color from that cateory
+//     * Say for reds:
+//         * Randomly pull a color from warms to color paint swatch
+//         * Randomly pull 3 color names + correct name for the options & 15 second timer
+//         * Randomly assign colors to 
+//             * Timer length may change after testing. May add option be toggled off 
+//         * If correct name is chosen
+//             * Score increases by 1
+//             * Option to stay in category or return to color wheel
+//             * Move correct guess into another array 
+                  // * have random guess checked against used color array -> if it's in there, choose a different color
+//         * Else 
+//             * Player loses that question. Score does not increase 
+//             * That color name is removed from options for the next round
+
+//             * Option to stay in category or return to color wheel 
+
+// * If the player plays all the way to the end, they get a percentage score pulled from their score
